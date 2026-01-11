f8d496e5d7635c9642711822020e23abbaeef574
smartacephale <atm.mormon@protonmail.com>
2026-01-11 02:16:26 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { JabronioGUI } from './app';
 export { setupScheme } from './scheme/default-scheme';
 export { JabronioStore } from './store';
-export type { SchemeInput } from './types';
+export type * as JabroniTypes from './types';
+export type { SchemeInput, StoreState } from './types';
 export { jabroniInit } from './utils/shorthand';
