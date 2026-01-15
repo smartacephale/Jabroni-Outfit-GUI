e7f7e023763d4f828403c7957652f4676ef93ff3
smartacephale <atm.mormon@protonmail.com>
2026-01-16 01:25:45 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,65 +2,40 @@ import path from 'node:path';
 import UnoCSS from 'unocss/vite';
 import Vue from 'unplugin-vue/vite';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const APP_VERSION = process.env.npm_package_version;
 
 export default defineConfig({
   plugins: [
     Vue({
-      // This tells Vue to compile .ce.vue files as Custom Elements
-      // content is inlined as strings rather than extracted to a global CSS file
       customElement: true,
     }),
     UnoCSS({
-      // CRITICAL: This mode tells UnoCSS to prepare CSS for Shadow DOM injection
       mode: 'shadow-dom',
     }),
+    dts({ rollupTypes: true }),
   ],
+  define: {
+    'process.env': {},
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
   build: {
+    // watch: false,
+    minify: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      name: 'MyWidget',
-      fileName: (format) => `my-widget.${format}.js`,
-      formats: ['es', 'umd'], // UMD is often better for simple script tags
+      entry: path.resolve(__dirname, './src/index.ts'),
+      name: 'jabronioutfit',
+      fileName: (format) => `jabroni-outfit.${format}.js`,
     },
-    // Ensure Vue is bundled if you want a standalone widget (no external dependencies)
+    sourcemap: true,
     rollupOptions: {
-      external: [], // Keep empty to bundle Vue inside the widget
+      external: [],
+      output: {
+        sourcemapBaseUrl: `https://cdn.jsdelivr.net/npm/jabroni-outfit@${APP_VERSION}/dist/`,
+      },
     },
   },
 });
-
-// import path from 'node:path';
-// import vue from '@vitejs/plugin-vue';
-// import UnoCSS from 'unocss/vite';
-// import { defineConfig } from 'vite';
-// import dts from 'vite-plugin-dts';
-
-// const APP_VERSION = process.env.npm_package_version;
-
-// export default () => {
-//   return defineConfig({
-//     plugins: [vue(), UnoCSS(), dts({ rollupTypes: true })],
-//     define: {
-//       'process.env': {},
-//       __VUE_OPTIONS_API__: false,
-//       __VUE_PROD_DEVTOOLS__: false,
-//       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-//     },
-//     build: {
-//       watch: false,
-//       minify: true,
-//       lib: {
-//         entry: path.resolve(__dirname, './src/index.ts'),
-//         name: 'jabronioutfit',
-//         fileName: (format) => `jabroni-outfit.${format}.js`,
-//       },
-//       sourcemap: true,
-//       rollupOptions: {
-//       external: ['uno.css'],
-//         output: {
-//           sourcemapBaseUrl: `https://cdn.jsdelivr.net/npm/jabroni-outfit@${APP_VERSION}/dist/`,
-//         },
-//       },
-//     },
-//   });
-// };
